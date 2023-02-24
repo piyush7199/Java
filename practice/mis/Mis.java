3237ba8af1ebb2bd7cfb1e2dc799862e7682df47
Piyush <maheswaripiyush9@gmail.com>
2023-02-24 14:50:01 +0530

    added code

--- practice/mis/Mis.java
+++ practice/mis/Mis.java
@@ -14,6 +14,30 @@ public class Mis {
         return res;
     }
 
+    public static long[] printFibb(int n)
+    {
+        long res[];
+        if(n == 1){
+            res = new long[1];
+            res[0]=1l;
+            return res;
+        }
+        if(n == 2){
+            res = new long[2];
+            res[0] = 1l;
+            res[1] = 1l;
+            return res;
+        }
+        res  = new long[n];
+        res[0] = 1l;
+        res[1] = 1l;
+        for(int i = 2;i<n;i++){
+            res[i] = res[i-1]+res[i-2];
+        }
+        return res;
+
+    }
+
     public static void main(String[] args) {
 
     }
--- practice/recursion/Recursion.java
+++ practice/recursion/Recursion.java
@@ -28,9 +28,28 @@ public class Recursion {
         printNos(N-1);
     }
 
+    public static boolean isPalindrome(String s){
+        int left = 0,right = s.length()-1;
+        while(right>left){
+            char l = s.charAt(left), r = s.charAt(right);
+            if(!Character.isLetterOrDigit(l)){
+                left ++;
+            }else if(!Character.isLetterOrDigit(r)){
+                right --;
+            }else if(Character.toLowerCase(l)!=Character.toLowerCase(r)){
+                return false;
+            }else{
+                left ++;
+                right --;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         // printNos(10);
         // printGfg(10);
         // printRevNos(10);
+        System.out.println(isPalindrome("Hey eh"));
     }
 }
